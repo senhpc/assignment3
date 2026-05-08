3f00fdf26d21e6099f9259e8b23cd24b8af2dc00
SenhPC-FX88656 <senhpcfx88656@funix.edu.vn>
2026-05-08 10:37:55 +0700

    fix a few bugs and create hover effect

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+"use strict";
+
 // ---- CHECK EMAIL ---- //
 
 const btnCheckEmail = document.querySelector(".btn-check-email");
-const infoContent = document.querySelector(".info-content");
+const infoContent = document.querySelector(".info-content-wrapper");
 const emailForm = document.querySelector("#email-form");
 const emailHelp = document.querySelector("#emailHelp");
-const inputEmail = document.querySelector("#InputEmail");
+const inputEmail = document.querySelector("#input-email");
 const regex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -13,36 +15,50 @@ btnCheckEmail.addEventListener("click", checkEmail);
 
 function checkEmail() {
   // check if the element exist
-  if (!inputEmail || !emailHelp) return;
+  if (!inputEmail) return;
 
   // if value is empty, or not match email regex then show error
-  if (!inputEmail.value) {
-    showError("Email không được để trống!!!");
-  } else if (regex.test(inputEmail.value)) {
-    emailForm.classList.add("hiding");
-    infoContent.classList.remove("hiding");
+  if (regex.test(inputEmail.value)) {
+    emailForm.classList.add("collapse-content");
+    infoContent.classList.remove("collapse-content");
   } else {
-    showError(`Email chưa đúng định dạng!`);
+    inputEmail.classList.add("is-invalid");
   }
 }
 
-function showError(message) {
-  emailHelp.textContent = message;
-  emailHelp.classList.add("error");
+// remove error status when user input
+inputEmail.addEventListener("input", () => {
+  inputEmail.classList.remove("is-invalid");
+});
 
-  setTimeout(() => {
-    emailHelp.textContent = "Hãy nhập email để xác thực!";
-    emailHelp.classList.remove("error");
-  }, 2000);
-}
+// press enter to check mail
+inputEmail.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    checkEmail();
+  }
+});
 
 // ---- BUTTON VIEW MORE ---- //
 
 // select all btn view more, change the inner html
-const viewMoreBtns = document.querySelectorAll(".btn-view-more");
+// const viewMoreBtns = document.querySelectorAll(".btn-view-more");
 
-viewMoreBtns.forEach((btn) => {
-  if (btn) {
-    btn.innerHTML = `<div class="text-slide"><span>View More</span><span>View Less</span></div>`;
-  }
+// viewMoreBtns.forEach((btn) => {
+//   if (btn) {
+//     btn.innerHTML = `<div class="text-slide"><span>View More</span><span>View Less</span></div>`;
+//   }
+// });
+
+const resume = document.querySelector("#resume");
+resume.addEventListener("click", (e) => {
+  // tìm nút view-more vừa click
+  const btn = e.target.closest(".btn-view-more");
+  if (!btn) return;
+  // tìm details-item chứa btn
+  const detailsItem = btn.closest(".details-item");
+  // tìm wrapper trong details-item và toggle class collapse-content
+  const contentWrapper = detailsItem.querySelector(".details-item-wrapper");
+  contentWrapper.classList.toggle("collapse-content");
+  btn.classList.toggle("is-collapsed");
 });
